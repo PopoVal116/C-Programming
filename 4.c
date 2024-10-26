83266358a4061d05641002c7a7132fe7a3a875e4
gc.gajco <valeriya.popova116@gmail.com>
2024-10-26 22:45:20 +0700

    Delete 4.c

--- 4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-        int a,s,n,k;
-        scanf("%d", &a);
-        for(n=a,s=0;n!=0;n=n/10){
-           k=n%10;
-           s+=k;}
-printf("сумма цифр числа а равна %d", s);
-}
