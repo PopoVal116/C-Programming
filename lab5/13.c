9511a7838949458c8905b8eb97b7b17ac4ae0105
gc.gajco <valeriya.popova116@gmail.com>
2024-10-30 03:12:02 +0200

    Add files via upload

---
+++ lab5/13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int step(unsigned int n) {
+    return (n > 0) && ((n & (n - 1)) == 0);
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    if (step(n)){
+        printf("%d является степенью числа 2\n", n);
+    }
+    else{
+        printf("%d не является степенью числа 2\n", n);
+    }
+}
