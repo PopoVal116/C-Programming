be59b7c72cbd62189d34a440b7d7b34d16033480
gc.gajco <valeriya.popova116@gmail.com>
2024-11-23 18:39:43 +0700

    Add files via upload

---
+++ lab10/3.c
@@ -0,0 +1,53 @@
+//Создайте структуру Прямоугольный_Параллелепипед с элементами 
+//– a и b – стороны прямоугольника-основания, h – высота
+// параллелепипеда. Найдите, у какого из N заданных 
+//параллелепипедов объем наименьший?
+
+#include <stdio.h>
+
+typedef struct {
+    int a; 
+    int b;
+    int h;
+} PP;
+
+
+int V(PP p) {
+    return p.a * p.b * p.h;
+}
+
+
+int minV(PP p[], int N) {
+    int k = 0; 
+    int minv = V(p[0]);
+
+    for (int i = 1; i < N; i++) {
+        int tV = V(p[i]);
+        if (tV < minv) {
+            minv = tV;
+            k = i;
+        }
+    }
+    return k;
+}
+
+int main() {
+    int N;
+    printf("Введите количество параллелепипедов: ");
+    scanf("%d", &N);
+
+    PP p[N]; 
+
+    for (int i = 0; i < N; i++) {
+        printf("Введите a, b, h для параллелепипеда %d: ", i + 1);
+        scanf("%d %d %d", &p[i].a, &p[i].b, &p[i].h);
+    }
+
+    int min = minV(p, N);
+
+    printf("Параллелепипед с наименьшим объемом: %d\n", min + 1); // +1 для удобства
+    printf("Стороны: a = %d, b = %d, h = %d\n", p[min].a, p[min].b, p[min].h);
+    printf("Объем: %d\n", V(p[min]));
+
+    return 0;
+}
